a7e0a74a6f198573d104c650fd9f4769ed3fcacc
midman001 <midlmancrypto@gmail.com>
2024-12-02 22:48:16 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
+import Wallet from './js/index.mjs';
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("satsConnect is:", window.satsConnect);
+    const wallet = new Wallet();
 
-    if (!window.satsConnect || typeof window.satsConnect.request !== "function") {
-        console.error("satsConnect is not available or improperly initialized.");
-        alert("Error: satsConnect library is not loaded. Please check your setup.");
-        return;
-    }
+    const connectButton = document.getElementById("connectButton");
 
     const connectToXverse = async () => {
         try {
-            const response = await window.satsConnect.request('wallet_connect', {
-                addresses: ['ordinals', 'payment', 'stacks'],
+            const response = await wallet.request("wallet_connect", {
+                addresses: ["ordinals", "payment", "stacks"],
                 message: "Connect your Xverse Wallet to MIDL Board Game",
             });
-            console.log("Response:", response);
+            console.log("Wallet connection response:", response);
         } catch (err) {
             console.error("Error during wallet connection:", err);
         }
     };
 
-    document.getElementById("connectButton").addEventListener("click", connectToXverse);
+    connectButton.addEventListener("click", connectToXverse);
 });
